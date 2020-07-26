f2820844ac46213baf9466c72d8d0c7f2266548a
Dmitry Palchynski <palchmail@gmail.com>
2020-07-26 12:51:09 +0300

    first

--- src/by/epamtc/calendar/Main.java
+++ src/by/epamtc/calendar/Main.java
@@ -1,12 +1,19 @@
 package by.epamtc.calendar;
 
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
       MyCalendar myCalendar=new MyCalendar();
-      myCalendar.year(2020);
+      myCalendar.printYear(2020);
+
     }
 
 
--- src/by/epamtc/calendar/MyCalendar.java
+++ src/by/epamtc/calendar/MyCalendar.java
@@ -1,23 +1,21 @@
 package by.epamtc.calendar;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Locale;
+import java.util.*;
 
 public class MyCalendar {
-   private String[][] calStr = new String[7][7];
-   private GregorianCalendar gregorianCalendar = new GregorianCalendar();
-   private GregorianCalendar day = new GregorianCalendar();
+    private GregorianCalendar gregorianCalendar = new GregorianCalendar();
+    private GregorianCalendar day = new GregorianCalendar();
 
 
-    public void month(int year, int month) {
+    public String[][] month(int year, int month) {
+        String[][] calStr = new String[7][7];
         gregorianCalendar.set(Calendar.YEAR, year);
-        gregorianCalendar.set(Calendar.MONTH, month-1);
+        gregorianCalendar.set(Calendar.MONTH, month - 1);
         gregorianCalendar.set(Calendar.DAY_OF_MONTH, 1);
         day.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
 
         for (int i = 0; i < calStr[0].length; i++) {
-            calStr[0][i] = day.getDisplayName(Calendar.DAY_OF_WEEK,Calendar.SHORT,Locale.US);
+            calStr[0][i] = day.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US);
             day.add(Calendar.DAY_OF_WEEK, 1);
         }
 
@@ -25,7 +23,7 @@ public class MyCalendar {
         for (int i = 1; i < calStr.length; i++) {
             for (int j = 0; j < calStr[i].length; j++) {
 
-                if (gregorianCalendar.getDisplayName(Calendar.DAY_OF_WEEK,Calendar.SHORT,Locale.US).equals(calStr[0][j]) &&
+                if (gregorianCalendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US).equals(calStr[0][j]) &&
                         temp < gregorianCalendar.getActualMaximum(Calendar.DAY_OF_MONTH)) {
 
                     calStr[i][j] = String.valueOf(gregorianCalendar.get(Calendar.DAY_OF_MONTH));
@@ -38,7 +36,21 @@ public class MyCalendar {
             }
         }
 
-        System.out.println("            "+gregorianCalendar.getDisplayName(Calendar.MONTH,Calendar.LONG, Locale.US));
+//
+        return calStr;
+    }
+
+    public String[][][] year(int year) {
+        String[][][] months = new String[12][7][7];
+        for (int i = 1; i <= 12; i++) {
+            months[i - 1] = month(year, i);
+        }
+        return months;
+    }
+
+    public void printMonth(int year, int month) {
+        String[][] calStr = month(year, month);
+        System.out.println("            " + gregorianCalendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US));
         for (String[] str : calStr) {
             for (String s : str) {
                 System.out.printf("%4s", s);
@@ -47,9 +59,30 @@ public class MyCalendar {
         }
     }
 
-    public void year(int year){
-        for (int i=1;i<=12;i++){
-            month(year,i);
+    public void printYear(int year) {
+        String[][][] months = year(year);
+
+        for (int i = 0; i < 12; i = i + 4) {
+            gregorianCalendar.set(Calendar.MONTH,i);
+            for (int monthNumber=i;monthNumber<i+4;monthNumber++) {
+                System.out.printf( "%24s",gregorianCalendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US));
+                System.out.printf("%3s"," ");
+                gregorianCalendar.roll(Calendar.MONTH,1);
+            }
+            System.out.println();
+                      for (int row = 0; row < 7; row++) {
+                for (int month = i; month < i+4; month++) {
+
+                    for (int col = 0; col < 7; col++) {
+                        System.out.printf("%4s", months[month][row][col]);
+                    }
+                    System.out.printf("%3s", " ");
+
+                }
+                System.out.println();
+            }
+            System.out.println();
         }
     }
+
 }
